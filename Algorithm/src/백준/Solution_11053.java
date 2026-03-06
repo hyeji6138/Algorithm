68693bfb12a4307abbfaccf9ef32b27a694d650e
hyeji6138 <hyeji6138@naver.com>
2026-03-06 16:05:13 +0900

    2026-03-06: 백준 11053 가장 긴 증가하는 부분 수열

---
+++ Algorithm/src/백준/Solution_11053.java
@@ -0,0 +1,31 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_11053 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		int[] dp = new int[N];
+		int answer = 0;
+
+		for (int i = 0; i < N; i++) {
+			dp[i] = 1;
+			arr[i] = Integer.parseInt(st.nextToken());
+
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+			answer = Math.max(answer, dp[i]);
+		}
+		System.out.println(answer);
+	}
+}
